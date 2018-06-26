adfca2d4c2c95a9b62a09ae9fad3fcbad7e64d4f
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 17:15:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,8 +43,13 @@ class Student
   end
 
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
   def self.create(name, grade)
