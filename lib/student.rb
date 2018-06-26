26e25ba0c7a68b5fb03b6dc05bd91040c5d5cca2
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 16:35:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade, :id
+  attr_accessor :id, :name, :grade
+
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
 
 
