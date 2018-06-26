640e0c604f700db90c7eef61121a1ae4d60ef7a5
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 17:20:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,8 +59,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(row[1], row[2], row[0])
-    student
+    # student = self.new(row[1], row[2], row[0])
+    # student
   end
 
   def self.find_by_name(name)
