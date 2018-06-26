dcc819f4ce82953f08f2828debc538065b072247
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 16:57:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
     student
   end
 
-  def self.new_from_db(row)
+  def self.new_from_db(id, name, grade)
     student = self.new
     student.id = row[0]
     student.name = row[1]
